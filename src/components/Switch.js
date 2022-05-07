953465cef7272ca6d82d83d21bf32618744edb0f
Gianfranco Caneva <gfocaneva@gmail.com>
2022-05-06 21:01:21 -0300

    Generalization of switch component

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,10 +2,10 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
-export default function SwitchLabels() {
+export default function SwitchLabels(props) {
     return (
       <FormGroup>
-        <FormControlLabel control={<Switch defaultChecked />} label="Second Factor Authentication (SFA):" />
+        <FormControlLabel control={<Switch defaultChecked />} label={props.text} />
       </FormGroup>
     );
   }
