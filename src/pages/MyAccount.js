64f3956be11622ce7448496c794994093f9402cd
Gianfranco Caneva <gfocaneva@gmail.com>
2022-05-08 19:40:53 -0300

    Adding footer to MyAccount

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import ListBar from '../components/sidebar/Listbar';
+import Footer from '../components/Footer';
 
 export default function MyAccount(props){
     const userName = props.fullUser.firstName + props.fullUser.lastName
@@ -12,6 +13,7 @@ export default function MyAccount(props){
         <>
             <Navbar text="Settings" userName={userName} pic={userPic} />
             <ListBar settingsOption={props.settingsOption} user={currentUser}></ListBar>
+            <Footer />
         </>
         );
     }
